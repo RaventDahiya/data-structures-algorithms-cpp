34644f7280bb8c0c370e0cba0eb08c23d5d79ba0
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-01 23:56:36 +0530

    Time: 35 ms (38.56%), Space: 47.7 MB (20.33%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> mp(26, 0);
+        for (auto& ch : tasks) {
+            mp[ch - 'A']++;
+        }
+        int time = 0;
+        priority_queue<int> pq;
+
+        for (auto& i : mp) {
+            if (i > 0)
+                pq.push(i);
+        }
+        while (!pq.empty()) {
+            vector<int> temp;
+            for (int i = 1; i <= n + 1; i++) {
+                if (!pq.empty()) {
+                    int freq = pq.top(); pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+            for(auto &f : temp){
+                if(f>0) pq.push(f);
+            }
+            if(pq.empty()){
+                time += temp.size();
+            }else{
+                time += n+1;
+            }
+        }
+        return time;
+    }
+};
