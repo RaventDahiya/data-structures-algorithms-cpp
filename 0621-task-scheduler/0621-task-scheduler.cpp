a6c89691fc9f261762d977e7d3e82253c15ef8db
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-02 00:06:08 +0530

    Time: 43 ms (21.98%), Space: 47.7 MB (31.67%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> mp(26, 0);
-        for (auto& ch : tasks) {
-            mp[ch - 'A']++;
+        vector<int>mp(26,0);
+        for(auto &ch : tasks){
+            mp[ch-'A']++;
+        }
+        priority_queue<int>pq;
+        for(auto &freq : mp){
+            if(freq>0) pq.push(freq);
         }
         int time = 0;
-        priority_queue<int> pq;
-
-        for (auto& i : mp) {
-            if (i > 0)
-                pq.push(i);
-        }
-        while (!pq.empty()) {
-            vector<int> temp;
-            for (int i = 1; i <= n + 1; i++) {
-                if (!pq.empty()) {
-                    int freq = pq.top(); pq.pop();
-                    freq--;
-                    temp.push_back(freq);
+        while(!pq.empty()){
+            vector<int>temp;
+            for(int i=1;i<=n+1;i++){
+                if(!pq.empty()){
+                    auto front = pq.top(); pq.pop();
+                    front--;
+                    temp.push_back(front);
                 }
             }
-            for(auto &f : temp){
-                if(f>0) pq.push(f);
+            for(auto &freq : temp){
+                if(freq>0) pq.push(freq);
             }
             if(pq.empty()){
                 time += temp.size();
