cfd780c921d66e2deaac0ab8e840cb8221fe8e48
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-04 00:48:57 +0530

    Time: 6 ms (19.51%), Space: 17.9 MB (84.41%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int>&mp;
         custom(unordered_map<int,int>&mapRef) : mp(mapRef) {}
         bool operator()(int a, int b){
-            return mp[a] < mp[b];
+            return mp[a] > mp[b];
         }
     };
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -15,7 +15,14 @@ public:
         priority_queue<int,vector<int>,custom>pq((custom(mp)));
 
         for(auto&[key,value]:mp){
-            pq.push(key);
+            if(pq.size()<k) pq.push(key);
+            else{
+                int front = pq.top();
+                if(mp[front] < value){
+                    pq.pop();
+                    pq.push(key);
+                }
+            }
         }
         vector<int>ans;
         while(k){
