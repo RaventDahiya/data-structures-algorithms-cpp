a49a7f4a5bfe908a8401f52f32c2a52fa6e5a695
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-06 01:07:30 +0530

    Time: 3 ms (27.14%), Space: 16.8 MB (70.12%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(int i,int j,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m) return false;
+        return true;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>>q;
+        int n = grid.size();
+        int m = grid[0].size();
+        int fresh = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1) fresh++;
+            }
+        }
+        if(fresh==0) return 0;
+        if(q.empty()) return -1;
+        int time = -1;
+        while(!q.empty()){
+            int size = q.size();
+            time++;
+            while(size--){
+                auto [i,j] = q.front(); q.pop();
+                int x[] = {0,-1,0,1};
+                int y[] = {-1,0,1,0};
+                for(int k=0;k<4;k++){
+                    int new_i = i + x[k];
+                    int new_j = j + y[k];
+                    if(isValid(new_i,new_j,n,m) && grid[new_i][new_j]==1){
+                        fresh--;
+                        grid[new_i][new_j]=2;
+                        q.push({new_i,new_j});
+                    }
+                }
+            }
+        }
+
+        if(fresh) return -1;
+        return time;
+    }
+};
