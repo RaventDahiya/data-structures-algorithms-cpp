a493aed567e3194646dd0ba67f6f3732cf950cba
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-08 01:54:49 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) { 
+        int zero = 0, one = 0, two = 0; 
+        for(auto i : nums){
+            if(i==0) zero++;
+            else if(i==1) one++;
+            else two++;
+        }
+        int i = 0;
+        while(zero--){
+            nums[i++] = 0;
+        }
+        while(one--){
+            nums[i++] = 1;
+        }
+        while(two--){
+            nums[i++] = 2;
+        }
+        
+    }
+};
