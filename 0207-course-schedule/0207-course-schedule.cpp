34c15f9f7bb870a5275a4004f9021773ade0fddb
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-21 17:41:11 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.14%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>ans;
+        vector<int>indegree(numCourses,0);
+        queue<int>q;
+        
+        vector<vector<int>>adj(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            
+            for(auto nbr : adj[node]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.push(nbr);
+            }
+        }
+        
+        if(ans.size()<numCourses) return false;
+        return true;
+    }
+};
