57c42ebfea97cfd717efeff0153ba62094a049c6
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-28 11:41:18 +0530

    Time: 97 ms (35.47%), Space: 24.4 MB (15.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,57 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>>q;
-        q.push({beginWord,1});
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        st.erase(beginWord);
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_map<string, int> mp;
+        wordList.insert(wordList.begin(), beginWord);
+        int len = beginWord.length();
+        int V = wordList.size();
+        for (int i = 0; i < V; i++) {
+            mp[wordList[i]] = i;
+        }
 
-        while(!q.empty()){
-            auto frontPair = q.front(); q.pop();
-            string frontString = frontPair.first;
-            int frontDistance = frontPair.second;
+        if (!mp[endWord])
+            return 0;
 
-            if(frontString==endWord) return frontDistance;
+        vector<vector<int>> adj(V);
 
-            for(int i=0;i<frontString.length();i++){
-                char toChange = frontString[i];
-                for(char ch='a' ; ch<='z' ; ch++){
-                    frontString[i] = ch;
-                    if(st.find(frontString)!=st.end()){
-                        q.push({frontString,frontDistance+1});
-                        st.erase(frontString);
-                    }
+        for (int i = 0; i < V; i++) {
+            string str = wordList[i];
+            for (int j = 0; j < len; j++) {
+                char original = str[j];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (ch == original) continue;
+                    str[j] = ch;
+                    if (mp.count(str)) adj[i].push_back(mp[str]);
                 }
-
-                frontString[i] = toChange;
-
+                str[j] = original; // Restore original character
             }
         }
 
-        return 0;
+        int src = 0;
+        int dest = mp[endWord];
+        vector<int> dis(V, INT_MAX);
+		dis[src] = 0;
+		
+		// BFS Queue
+		queue<int> q;
+		q.push(src);
+		
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			
+			for (auto nbr : adj[node]) {
+				if (dis[nbr] == INT_MAX) {
+					dis[nbr] = dis[node] + 1;
+					q.push(nbr);
+				}
+			}
+		}
+		
+		if (dis[dest] == INT_MAX)
+			return 0;
+		
+		return dis[dest] + 1;
     }
 };
