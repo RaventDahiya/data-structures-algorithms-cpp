686f23934e33f49623bd55e2e377c338609e0387
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-07-28 11:47:41 +0530

    Time: 40 ms (94.08%), Space: 21.1 MB (69.3%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,56 +2,35 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        unordered_map<string, int> mp;
-        wordList.insert(wordList.begin(), beginWord);
-        int len = beginWord.length();
-        int V = wordList.size();
-        for (int i = 0; i < V; i++) {
-            mp[wordList[i]] = i;
-        }
+        queue<pair<string, int>> q;
+        unordered_set<string> st(wordList.begin(), wordList.end());
 
-        if (!mp[endWord])
-            return 0;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
 
-        vector<vector<int>> adj(V);
+        while (!q.empty()) {
+            auto [word, steps] = q.front();
+            q.pop();
 
-        for (int i = 0; i < V; i++) {
-            string str = wordList[i];
-            for (int j = 0; j < len; j++) {
-                char original = str[j];
+            if (word == endWord)
+                return steps;
+
+            for (int i = 0; i < word.size(); i++) {
+                char original = word[i];
+
                 for (char ch = 'a'; ch <= 'z'; ch++) {
-                    if (ch == original) continue;
-                    str[j] = ch;
-                    if (mp.count(str)) adj[i].push_back(mp[str]);
+                    word[i] = ch;
+
+                    if (st.count(word)) {
+                        st.erase(word);
+                        q.push({word, steps + 1});
+                    }
                 }
-                str[j] = original; // Restore original character
+
+                word[i] = original;
             }
         }
 
-        int src = 0;
-        int dest = mp[endWord];
-        vector<int> dis(V, INT_MAX);
-		dis[src] = 0;
-		
-		// BFS Queue
-		queue<int> q;
-		q.push(src);
-		
-		while (!q.empty()) {
-			int node = q.front();
-			q.pop();
-			
-			for (auto nbr : adj[node]) {
-				if (dis[nbr] == INT_MAX) {
-					dis[nbr] = dis[node] + 1;
-					q.push(nbr);
-				}
-			}
-		}
-		
-		if (dis[dest] == INT_MAX)
-			return 0;
-		
-		return dis[dest] + 1;
+        return 0;
     }
 };
