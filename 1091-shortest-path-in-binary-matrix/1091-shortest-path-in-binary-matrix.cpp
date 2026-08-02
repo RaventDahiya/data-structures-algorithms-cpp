bda21ffa55a806c91326412fc0d0565a4d64497e
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-03 00:27:23 +0530

    Time: 11 ms (88.6%), Space: 25.3 MB (53.16%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
         int n = grid.size();
+
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
+
         vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        queue<pair<int, int>> q;
+
         dist[0][0] = 1;
-        set<pair<int, pair<int, int>>> st;
-        st.insert({1, {0, 0}}); // { wt , {row,col }}
+        q.push({0, 0});
+
         int x[] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-        while (!st.empty()) {
-            auto [dis, v] = *(st.begin());
-            auto [row, col] = v;
-            st.erase(st.begin());
-            if (dis > dist[row][col]) continue;
+
+        while (!q.empty()) {
+
+            auto [row, col] = q.front();
+            q.pop();
+
             for (int dir = 0; dir < 8; dir++) {
+
                 int newX = row + x[dir];
                 int newY = col + y[dir];
-                if (newX >= 0 && newY >= 0 && newX < n && newY < n &&
+
+                if (newX >= 0 && newY >= 0 &&
+                    newX < n && newY < n &&
                     grid[newX][newY] == 0) {
-                    if (dis + 1 < dist[newX][newY]) {
-                        if (dist[newX][newY] != INT_MAX) {
-                            st.erase({dist[newX][newY],{newX,newY}});
-                        }
-                        dist[newX][newY] = dis + 1;
-                        st.insert({dist[newX][newY], {newX,newY}});
+
+                    if (dist[row][col] + 1 < dist[newX][newY]) {
+
+                        dist[newX][newY] = dist[row][col] + 1;
+
+                        q.push({newX, newY});
                     }
                 }
             }
         }
-        if(dist[n-1][n-1]==INT_MAX) return -1;
-        return dist[n-1][n-1];
+
+        if (dist[n - 1][n - 1] == INT_MAX)
+            return -1;
+
+        return dist[n - 1][n - 1];
     }
 };
