6831a5bc452218acfc3ef1be2953f398b675531e
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-04 17:26:54 +0530

    Enhance DisjointSet with size tracking and unionBySize
    Added size vector to track the size of each component in the Disjoint Set. Implemented unionBySize method to union two sets based on their sizes.

--- src/graphs/DisjointSet.cpp
+++ src/graphs/DisjointSet.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 class DisjointSet {
-    vector<int> rank, parent;
+    vector<int> rank, parent,size;
 
 public:
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
-
+        size.resize(n+1,1);
         for (int i = 0; i <= n; i++) {
             parent[i] = i;
         }
@@ -41,6 +41,24 @@ public:
             rank[ulp_u]++;
         }
     }
+    void unionBySize(int u, int v) {
+
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        // Already in same component
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
 };
 
 int main() {
