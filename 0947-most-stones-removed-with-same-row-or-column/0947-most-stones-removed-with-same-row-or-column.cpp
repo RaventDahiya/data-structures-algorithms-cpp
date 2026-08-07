a955afaf6f6e000c6988cffe60073b5e93e24b09
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-07 23:00:05 +0530

    Time: 12 ms (78.57%), Space: 25.6 MB (11.99%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,86 @@
+class DisjointSet {
+    vector<int> rank, parent,size;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n+1,1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        // Already in same component
+        if (ulp_u == ulp_v)
+            return;
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        // Already in same component
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = 0;
+        int m = 0;
+        for(auto it : stones){
+            int i = it[0];
+            int j = it[1];
+            n = max(n,i);
+            m = max(m,j);
+        }
+        DisjointSet ds(n+m+1);
+        unordered_map<int,int>stoneNodes;
+        for(auto it : stones){
+            int i = it[0];
+            int j = it[1] +n+1;
+            ds.unionBySize(i,j);
+            stoneNodes[i] = 1;
+            stoneNodes[j] = 1;
+        }
+        int cnt = 0;
+        for(auto [it,t] : stoneNodes){
+            if(ds.findUPar(it) == it) cnt++;
+        }
+        return stones.size() - cnt;
+    }
+};
