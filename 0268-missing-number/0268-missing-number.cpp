24c380f067740ef76ea624deabb6609b252d7a03
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-11 16:24:26 +0530

    Time: 0 ms (100%), Space: 22 MB (12.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int>st;
-        for(auto& num : nums){
-            st.insert(num);
+        int i =0;
+        int n = nums.size();
+
+        while(i<n){
+            if(nums[i]==i || nums[i] == n){
+                i++;
+            }else{
+                swap(nums[i],nums[nums[i]]);
+            }
         }
-        for(int i=0;i<=nums.size();i++){
-            if(!st.count(i)) return i;
+
+        i = 0;
+        for(;i<n;i++){
+            if(nums[i]!=i) return i;
         }
-        return -1;
+        return n;
     }
 };
