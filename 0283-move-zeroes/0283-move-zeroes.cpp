d53dd5f6fbc3e9b1b98610648031a3f3a2c1fb87
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-16 17:55:13 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int s = 0;
-        int f = 0;
-        while (s < n && f < n) {
-            while (s < n && nums[s] != 0)
-                s++;
-            f = s + 1;
-            while (f < n && nums[f] == 0)
-                f++;
-            if (f < n && s < n) {
+        int s = 0;                  // position to place next non-zero
+
+        for (int f = 0; f < nums.size(); f++) {
+            if (nums[f] != 0) {
                 swap(nums[s], nums[f]);
                 s++;
-                f++;
             }
         }
     }
