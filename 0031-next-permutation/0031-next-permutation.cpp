43506b8b74d07593b48a6f111047ba8eb6a82740
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-21 20:23:05 +0530

    Time: 0 ms (100%), Space: 15.7 MB (53.85%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size();
+        int breakingPoint = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                breakingPoint = i;
+                break;
+            }
+        }
+        if(breakingPoint == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i=n-1;i>=breakingPoint+1;i--){
+            if(nums[i] > nums[breakingPoint]){
+                swap(nums[i],nums[breakingPoint]);
+                reverse(nums.begin()+breakingPoint+1,nums.end());
+                return;
+            }
+        }
     }
 };
