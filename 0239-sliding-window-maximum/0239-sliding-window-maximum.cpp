03826d97a45170d86f4272ca99246c195e5344e3
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-31 00:31:00 +0530

    Time: 20 ms (69.91%), Space: 139.1 MB (74.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
         vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            //remove out of the window ele
-            while(!dq.empty() && dq.front() <= i-k){
+        deque<int>dq; //monotonic decreasing dq
+        int left = 0;
+
+        for(int right=0;right<nums.size();right++){
+            //remove out of bound
+            while(!dq.empty() && dq.front() <= right-k){
                 dq.pop_front();
             }
-            //maintain max dq
-            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+
+            //maintain monotonic decreasing dq
+            while(!dq.empty() && nums[dq.back()] <= nums[right]){
                 dq.pop_back();
             }
+
             //add new ele
-            dq.push_back(i);
-            //store ans
-            if(i >= k-1)
-            ans.push_back(nums[dq.front()]);
+            dq.push_back(right);
+
+            //add ans 
+            if(right >= k-1){
+                ans.push_back(nums[dq.front()]);
+            }
         }
+
         return ans;
+
     }
 };
