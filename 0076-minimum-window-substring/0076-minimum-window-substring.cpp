3c973265aa20702f876b5c847aba47311c2028fb
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-31 00:20:26 +0530

    Time: 22 ms (17.25%), Space: 11.5 MB (43.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.size()>s.size()) return "";
-        unordered_map<int,int>need;
-        unordered_map<int,int>window;
-        for(auto& ch:t){
+        if (t.length() > s.length())
+            return "";
+        unordered_map<char, int> need;
+        for (auto& ch : t) {
             need[ch]++;
         }
         int req = need.size();
         int formed = 0;
-
+        unordered_map<char, int> window;
         int minLen = INT_MAX;
         int start = -1;
         int left = 0;
 
-
-        for(int right=0;right<s.length();right++){
+        for (int right = 0; right < s.length(); right++) {
             char currCh = s[right];
-            if(need.count(currCh)){
+            if (need.count(currCh)) {
                 window[currCh]++;
-                if(window[currCh] == need[currCh]) formed++;
+                if (need[currCh] == window[currCh]) formed++;
             }
 
-            while(req == formed){
-                if(right-left+1 < minLen){
-                    minLen = right-left+1;
+            while (formed == req) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     start = left;
                 }
                 char prevCh = s[left];
-                if(need.count(prevCh)){
+                if (need.count(prevCh)) {
                     window[prevCh]--;
-                    if(window[prevCh] < need[prevCh]) formed--;
+                    if (need[prevCh] > window[prevCh]) formed--;
                 }
                 left++;
             }
         }
 
-        if(start==-1) return "";
-        return s.substr(start,minLen);
+        return start == -1 ? "" : s.substr(start, minLen);
     }
 };
