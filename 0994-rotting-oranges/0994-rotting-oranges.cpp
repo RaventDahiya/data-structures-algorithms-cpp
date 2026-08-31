767c499961feb795a7d289d8ad3e608279358723
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-31 23:53:10 +0530

    Time: 3 ms (25.56%), Space: 16.7 MB (69.46%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-    bool isValid(int i,int j,int n,int m){
-        if(i<0 || i>=n || j<0 || j>=m) return false;
-        return true;
-    }
     int orangesRotting(vector<vector<int>>& grid) {
+        int freshCount = 0;
         queue<pair<int,int>>q;
-        int n = grid.size();
-        int m = grid[0].size();
-        int fresh = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
-                else if(grid[i][j]==1) fresh++;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) q.push({i,j});
+                else if(grid[i][j]==1) freshCount++;
             }
         }
-        int x[4] = {0,-1,0,1};
-        int y[4] = {-1,0,1,0};
-        if(fresh==0) return 0;
-        if(q.empty()) return -1;
-        int time = -1;
-        while(!q.empty()){
+        int time = 0;
+        int rottenedCount = 0;
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,-1,1};
+        while(!q.empty() && freshCount>0){
             int size = q.size();
-            
-            while(size--){
-                auto [i,j] = q.front(); q.pop();
-                for(int k=0;k<4;k++){
-                    int new_i = i + x[k];
-                    int new_j = j + y[k];
-                    if(isValid(new_i,new_j,n,m) && grid[new_i][new_j]==1){
-                        fresh--;
-                        grid[new_i][new_j]=2;
-                        q.push({new_i,new_j});
+            for(int i=0;i<size;i++){
+                auto [row,col] = q.front(); q.pop();
+                for(int dir=0;dir<4;dir++){
+                    int newRow = row + dx[dir];
+                    int newCol = col + dy[dir];
+                    if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]==1){
+                        grid[newRow][newCol]=2;
+                        freshCount--;
+                        q.push({newRow,newCol});
                     }
+
                 }
             }
             time++;
         }
-
-        if(fresh) return -1;
-        return time;
+        if(freshCount<=0) return time;
+        return -1;
     }
 };
