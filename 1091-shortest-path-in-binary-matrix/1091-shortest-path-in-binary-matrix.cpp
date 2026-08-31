3cbe672091f432be78a413f5f197c303fd22bbbd
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-08-31 20:49:40 +0530

    Time: 21 ms (41.43%), Space: 23.8 MB (75.68%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
         int n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+        if (grid[0][0] == 1)
             return -1;
-
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-
+        if (n == 1)
+            return 1;
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
         queue<pair<int, int>> q;
-
-        dist[0][0] = 1;
+        int step = 1;
         q.push({0, 0});
-
-        int x[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
+        vis[0][0] = true;
+        int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
         while (!q.empty()) {
+            int size = q.size();
+            while (size) {
+                auto [row, col] = q.front();
+                q.pop();
+                if (row == n - 1 && col == n - 1)
+                    return step;
 
-            auto [row, col] = q.front();
-            q.pop();
+                for (int i = 0; i < 8; i++) {
+                    int newRow = row + dx[i];
+                    int newCol = col + dy[i];
 
-            for (int dir = 0; dir < 8; dir++) {
-
-                int newX = row + x[dir];
-                int newY = col + y[dir];
-
-                if (newX >= 0 && newY >= 0 &&
-                    newX < n && newY < n &&
-                    grid[newX][newY] == 0) {
-
-                    if (dist[row][col] + 1 < dist[newX][newY]) {
-
-                        dist[newX][newY] = dist[row][col] + 1;
-
-                        q.push({newX, newY});
+                    if (newRow >= 0 && newRow < n && newCol >= 0 &&
+                        newCol < n && !grid[newRow][newCol] &&
+                        !vis[newRow][newCol]) {
+                        vis[newRow][newCol] = true;
+                        q.push({newRow, newCol});
                     }
                 }
+                size--;
             }
+            step++;
         }
 
-        if (dist[n - 1][n - 1] == INT_MAX)
-            return -1;
-
-        return dist[n - 1][n - 1];
+        return -1;
     }
 };
