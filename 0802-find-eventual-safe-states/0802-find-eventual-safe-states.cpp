e9f1143888a5a41abef3ae48964de020a9388093
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-02 01:08:58 +0530

    Time: 12 ms (66.55%), Space: 51.7 MB (92.13%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<vector<int>> adjRev(n);
-        vector<int>indegree(n,0);
-
-        for(int i=0;i<n;i++){
-            for(auto nbr : graph[i]){
-                adjRev[nbr].push_back(i);
-                indegree[i]++;
+    bool dfs(int node,vector<vector<int>>& g,vector<int>&state){
+        if(state[node]==2) return true; //already processed
+        if(state[node]==3) return false;//already processed
+        if(state[node]==1) return false; //cycle found
+        state[node] = 1;
+        for(auto nbr:g[node]){
+            if(!dfs(nbr,g,state)){
+                state[node] = 3;
+                return false;
             }
         }
+        state[node] = 2;
+        return true;
 
-        queue<int>q;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& g) {
+        int n = g.size();
+        vector<int>state(n,0); //0:unvis 1:currInPath 2:safe 3:unsafe
         vector<int>safeNodes;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.push(i);
+            if(dfs(i,g,state)) safeNodes.push_back(i);
         }
-
-        while(!q.empty()){
-            int node = q.front(); q.pop();
-            safeNodes.push_back(node);
-            for(auto nbr : adjRev[node]){
-                indegree[nbr]--;
-                if(indegree[nbr]==0) q.push(nbr);
-            }
-        }
-
-        sort(safeNodes.begin(),safeNodes.end());
         return safeNodes;
     }
 };
