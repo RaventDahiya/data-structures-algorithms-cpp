452c7d994e449d6a665623455f7bba19d0f751b5
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-01 20:42:30 +0530

    Time: 51 ms (75.14%), Space: 21.3 MB (41.76%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        queue<pair<string,int>>q;
-        q.push({beginWord,1});
-
-        while(!q.empty()){
-            auto [str,dis] = q.front(); q.pop();
-            if(str==endWord) return dis;
-            
-            for(int i=0;i<str.length();i++){
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue < pair<string, int>> q;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
+        while (!q.empty()) {
+            auto [str,dist] = q.front(); q.pop();
+            if(str==endWord) return dist;
+            for (int i = 0; i < str.size(); i++) {
                 char org = str[i];
-                for(char ch='a';ch<='z';ch++){
-                    str[i] = ch;
-                    if(st.count(str)){
-                        q.push({str,dis+1});
-                        st.erase(str);
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (ch != org) {
+                        str[i] = ch;
+                        if (st.count(str)) {
+                            q.push({str,dist+1});
+                            st.erase(str);
+                        }
                     }
+                    str[i] = org;
                 }
-                str[i] = org;
             }
-            
         }
-
         return 0;
     }
 };
