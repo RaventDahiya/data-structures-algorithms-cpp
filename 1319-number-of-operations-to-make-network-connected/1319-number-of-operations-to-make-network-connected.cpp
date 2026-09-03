10c57fd09ed91c2321bc2db86c7167d918262929
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-03 13:31:19 +0530

    Time: 24 ms (34.37%), Space: 47.2 MB (32.05%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,84 +1,72 @@
-class DisjointSet {
-    vector<int> rank, parent,size;
-
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n+1,1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
+class DisjointSet{
+    vector<int>rank,size,parent;
+    public:
+        DisjointSet(int n){
+            rank.resize(n+1,0);
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++) parent[i] = i;
         }
-    }
 
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        // Already in same component
-        if (ulp_u == ulp_v)
-            return;
-
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
+        int findUpar(int node){
+            if(node == parent[node]) return node;
+            return parent[node] = findUpar(parent[node]);
         }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-    void unionBySize(int u, int v) {
 
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
+        void unionByRank(int u,int v){
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
+            if(ulp_u == ulp_v) return;
 
-        // Already in same component
-        if (ulp_u == ulp_v)
-            return;
-
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+            if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }else if(rank[ulp_v] < rank[ulp_u]){
+                parent[ulp_v] = ulp_u;
+            }else{
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
         }
-    }
-    
+
+        void unionBysize(int u,int v){
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
+            if(ulp_u == ulp_v) return;
+
+            if(size[ulp_u] < size[ulp_v]){
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            }else{
+                parent[ulp_v] = ulp_u;
+                size[ulp_u] += size[ulp_v];
+            }
+        }
 };
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         int totalEdges = connections.size();
+        int requiredEdges = n - 1;
+        if(requiredEdges > totalEdges) return -1;
+
+        
+
         DisjointSet ds(n);
-        int edgesUsed = 0;
+        //build ds
         for(auto it : connections){
             int u = it[0];
             int v = it[1];
-            if(ds.findUPar(u) != ds.findUPar(v)){
-                edgesUsed++;
-                ds.unionBySize(u,v);
+            ds.unionByRank(u,v);
+        }
+        
+        //count no of disconnected comp
+        int noOfComp = 0;
+        for(int i=0;i<n;i++){
+            if(ds.findUpar(i)==i){
+                noOfComp++;
             }
         }
-        int edgesLeft = totalEdges - edgesUsed;
 
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(ds.findUPar(i)==i) count++;
-        }
-        if(count - 1 <= edgesLeft) return count-1;
-        return -1;
-
+        return noOfComp - 1;
     }
 };
