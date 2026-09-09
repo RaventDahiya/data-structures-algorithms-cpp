f3b8b014edcb830acde01c4e38c8050999893211
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-09 13:30:33 +0530

    Time: 16 ms (31.82%), Space: 28.6 MB (26.31%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -3,30 +3,23 @@ public:
     int maxProduct(vector<string>& words) {
         int maxi = 0;
         int n = words.size();
+        vector<int> mask(n, 0);
 
-        for(int i=0;i<n-1;i++){
-            string str1 = words[i];
-            unordered_set<char>st;
-    
-            for(auto ch : str1){
-                st.insert(ch);
+        for (int i = 0; i < n ; i++) {
+            string str = words[i];
+            for (auto ch : str) {
+                int pos = ch - 'a';      // char index/pos
+                int bit = 1 << pos;      // shift the pos
+                mask[i] = mask[i] | bit; // or is used to add
             }
-            int len1 = str1.length();
-            bool commonLetter = false;
-            for(int j=i+1;j<n;j++){
-                string str2 = words[j];
-                for(auto ch : str2){
-                    if(st.count(ch)){ //common letter found
-                        commonLetter = true;
-                        break;
-                    }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((mask[i] & mask[j]) == 0) {
+                    int product = words[i].length() * words[j].length();
+                    maxi = max(maxi, product);
                 }
-                if(!commonLetter){
-                    
-                    int len2 = str2.length();
-                    maxi = max(maxi,len1*len2);
-                }
-                commonLetter = false;
             }
         }
         return maxi;
