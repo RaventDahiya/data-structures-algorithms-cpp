7659767c07d0d4fc52666ffa95b32d51602c5075
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-13 20:24:27 +0530

    Time: 0 ms (100%), Space: 95.2 MB (60.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        if (n > m)
-            return findMedianSortedArrays(nums2, nums1);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
 
-        int half = (n + m + 1) / 2;
-        int left = 0;
-        int right = n;
+        // Binary search on smaller array
+        if (a.size() > b.size())
+            return findMedianSortedArrays(b, a);
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int l1 = mid - 1 >= 0 ? nums1[mid - 1] : INT_MIN;
-            int l2 = half - mid - 1 >= 0 ? nums2[half - mid - 1] : INT_MIN;
-            int r1 = mid < n ? nums1[mid] : INT_MAX;
-            int r2 = half - mid < m ? nums2[half - mid] : INT_MAX;
+        int n = a.size();
+        int m = b.size();
 
+        int low = 0;
+        int high = n;
+
+        while (low <= high) {
+
+            int mid1 = low + (high - low) / 2;
+            int mid2 = (n + m + 1) / 2 - mid1;
+
+            int l1 = (mid1 == 0) ? INT_MIN : a[mid1 - 1];
+            int r1 = (mid1 == n) ? INT_MAX : a[mid1];
+
+            int l2 = (mid2 == 0) ? INT_MIN : b[mid2 - 1];
+            int r2 = (mid2 == m) ? INT_MAX : b[mid2];
+
+            // Correct partition
             if (l1 <= r2 && l2 <= r1) {
-                if ((n + m) % 2 == 1) return (double)max(l1, l2);
-                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
-            } else if (l1 > r2) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+
+                if ((n + m) % 2 == 1) {
+                    return max(l1, l2);
+                }
+
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+
+            // Too many elements taken from a
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+
+            // Too few elements taken from a
+            else {
+                low = mid1 + 1;
             }
         }
+
         return 0;
     }
 };
