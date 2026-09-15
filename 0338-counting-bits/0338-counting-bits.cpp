18a310c911abaa3ccc272bbdd498a8d075b64482
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-15 12:00:27 +0530

    Time: 0 ms (100%), Space: 11.4 MB (8%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countBitsHelper(int n){
+        int count = 0;
+        while(n){
+            n = n & n-1;
+            count++;
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(countBitsHelper(i));
+        }
+        return ans;
+    }
+};
