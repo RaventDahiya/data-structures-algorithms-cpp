9b2332f5900d5e6f49f0ccc407d195281da7eb33
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-21 16:29:59 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.98%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,20 +3,13 @@ public:
     int maxProduct(vector<int>& nums) {
         int maxi = nums[0];
         int mini = nums[0];
-
         int ans = nums[0];
-
         for(int i=1;i<nums.size();i++){
-            int num = nums[i];
-
-            if(num < 0) swap(maxi,mini);
-
-            maxi = max(num,num*maxi);
-            mini = min(num,num*mini);
-
-            ans = max(maxi,ans);
+            if(nums[i]<0) swap(maxi,mini);
+            maxi = max(nums[i],nums[i]*maxi);
+            mini = min(nums[i],nums[i]*mini);
+            ans = max(ans,maxi);
         }
-
         return ans;
     }
 };
