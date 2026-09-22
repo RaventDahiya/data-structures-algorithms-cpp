d55468da850915f28ef9ba08208220fe06944a91
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-22 14:14:12 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.6%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& a) {
-        int l = 0, r = a.size() - 1;
-        int n = a.size();
-        if (a.size() == 1)
-            return a[0];
-        if (a[0] != a[1])
-            return a[0];
-        if (a[n - 1] != a[n - 2])
-            return a[n - 1];
-            
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        // handle ege cases
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int l = 1;
+        int r = n - 2;
+
         while (l <= r) {
             int m = l + (r - l) / 2;
+            if (nums[m] != nums[m + 1] && nums[m] != nums[m - 1])
+                return nums[m];
 
-            if (a[m] != a[m + 1] && a[m] != a[m - 1])
-                return a[m];
-
-            if (a[m] == a[m - 1]) { // left match
-                int len = m + 1;
-                if (len % 2 == 0) { // even len single ele can not be here
-                    l = m + 1;
+            if (nums[m] == nums[m + 1]) {
+                if (m % 2 == 0) { // even
+                    l = m + 2;
                 } else {
                     r = m - 1;
                 }
-            } else { // right match
-                int len = a.size() - m;
-                if (len % 2 == 0) { // even len single ele can not be here
-                    r = m - 1;
+            } else {
+                if (m % 2 == 0) { // even
+                    r = m - 2;
                 } else {
                     l = m + 1;
                 }
             }
         }
-        return a[r];
+        return -1;
     }
 };
