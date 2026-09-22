94f895ddf71b8c06683379125b5d22345521122b
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-22 15:43:43 +0530

    Time: 18 ms (84.02%), Space: 70.5 MB (80.52%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    bool isValid(vector<int>& bloomDay, int m, int k,int days){
-        int totalFlower = 0;
-        int adjFlower = 0;
-        for(auto it : bloomDay){
-            if(days >= it){ //flower bloomed
-                adjFlower++;
-                if(adjFlower == k){ //flower made
-                    totalFlower++;
-                    adjFlower = 0;
-                    if(totalFlower==m) return true;
+    bool isValid(vector<int>& b, int m, int k,int days){
+        int bouquetsCount = 0;
+        int flowerbBloom = 0;
+
+        for(auto & f : b){
+            if(f<=days){
+                flowerbBloom++;
+                if(flowerbBloom==k){
+                    bouquetsCount++;
+                    flowerbBloom=0;
                 }
             }else{
-                adjFlower = 0;
+                flowerbBloom=0;
             }
         }
-        if(totalFlower>=m) return true;
-        return false;
+
+        return bouquetsCount>=m;
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        if(bloomDay.size() < (long long)m*k) return -1;
+    int minDays(vector<int>& b, int m, int k) {
+        int n = b.size();
+        if(n < (long long)m * k) return -1;
 
-        int left = 0; 
-        int right = INT_MIN;
-
-        for(auto it : bloomDay){
-            right = max(right,it);
-        }
-
-        int minDay = right;
-
-        while(left <= right){
-            int mid = left + (right-left)/2;
-
-            if(isValid(bloomDay,m,k,mid)){
-                minDay = mid;
-                right = mid - 1;
+        int l = 1;
+        int r = *max_element(b.begin(),b.end());
+        int minDays = INT_MAX;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(isValid(b,m,k,mid)){
+                minDays = min(minDays,mid);
+                r = mid - 1;
             }else{
-                left = mid + 1;
+                l = mid + 1;
             }
         }
-        return minDay;
+        return minDays==INT_MAX ? -1 : minDays;
     }
 };
