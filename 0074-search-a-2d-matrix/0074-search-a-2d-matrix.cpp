3189e63924168516ee14240a28df1131063721dc
Ravent Dahiya <raventdahiya2003@gmail.com>
2026-09-23 10:58:04 +0530

    Time: 0 ms (100%), Space: 13.3 MB (43.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
+    bool searchMatrix(vector<vector<int>>& mat, int t) {
         int n = mat.size();
         int m = mat[0].size();
 
-        int left = 0;
-        int right = (n * m) - 1;
+        int l = 0;
+        int r = (n * m) - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
             int i = mid / m;
             int j = mid % m;
-            if(mat[i][j]==target){
-                return true;
-            }else if(mat[i][j]>target){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
+
+            if (mat[i][j] == t) return true;
+
+            if (mat[i][j] > t)
+                r = mid - 1;
+            else
+                l = mid + 1;
         }
 
         return false;
